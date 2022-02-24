bd0635c914a3b04fedd8a1f17223c7f91ec1ff74
Lonna Barlow <lonna@lemonblock.com>
2022-02-24 12:06:20 -0700

    restart

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const path = require("path")
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
+    res.sendFile(path.join(__dirname, "public/index.html"))
 
 })
 
 const port = process.env.PORT || 4545
 
-app.listen(port, () => console.log("Take us to warp ${port}"))
+app.listen(port, () => console.log(`Take us to warp ${port}`))
